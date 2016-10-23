22714cf10e9a57b46ae29dfd854e5358085f034d
erik4281 <erik4281@users.noreply.github.com>
2016-10-23 16:39:38 +0200

    Create Sensor.py

---
+++ Sensor.py
@@ -0,0 +1,19 @@
+import urllib2
+import time
+
+def SensorState( SensorName ):
+
+  s = "buttonevent\":"
+  a = urllib2.urlopen("http://10.0.1.102/api/monitor/sensors/" + SensorName ).read()
+  b =  a.find(s) + len(s)
+  e = a.find(",",b)
+
+  return a[b:e];
+
+while 1 == 1:
+
+ lastState = SensorState("2")
+ while (SensorState ("2") == lastState):
+   time.sleep(0.5)
+
+ print "State Changed!" + SensorState("2")
