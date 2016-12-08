1d4c07ea619c547b93f7067c6988a95b2dc1f1f9
erik4281 <erik4281@users.noreply.github.com>
2016-12-08 20:58:07 +0100

    Update Hue.sh

--- Hue.sh
+++ Hue.sh
@@ -25,8 +25,7 @@ HOME_OLD=${HOME_STATE}
 MOTION_OLD=${MOTION_STATE}
 
 #CHECK_LIGHTS=curl -s -silent -H "Accept: application/json" -X GET http://${HUE_IP}/api/${HUE_USER}/lights/ | grep '":{"state":{"on":true'
-CHECK_LIGHTS=$(curl -s -silent -H "Accept: application/json" -X GET http://${HUE_IP}/api/${HUE_USER}/lights/ | grep '":{"state":{"on":true')
-echo IP ${HUE_IP}
+CHECK_LIGHTS=$(curl -s -silent -H "Accept: application/json" -X GET http://${HUE_IP}/api/${HUE_USER}/lights/34/ | grep '":{"state":{"on":true')
 echo Lights Checker ${CHECK_LIGHTS}
 
 if [ $? -eq 0 ]; then
