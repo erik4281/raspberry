c61762e42179a0c78c9a8ec42ebf47e3e826c253
erik4281 <erik4281@users.noreply.github.com>
2016-12-31 19:35:08 +0100

    Update Hue.sh

--- Hue.sh
+++ Hue.sh
@@ -4,19 +4,15 @@ DELAY_BETWEEN_CHECKS=15
 sleep 10
 
 HUE_IP=$(curl https://www.meethue.com/api/nupnp 2> /dev/null | jq -r ".[0].internalipaddress")
-#HUE_IP=192.168.1.11
 HUE_USER=vSuYBJAton1scEhPyDf4ep85GgmRyFvjJfBjYHIC
 
 PUSHOVER_TOKEN=azw2c2dw29x8o96ae2m2cp5gtx7mr4
 PUSHOVER_USER=uPnTmp5puvngjiUYpGMGQ3AgUKjhgZ
-#PUSHOVER_ALARM_TITLE="ALARM"
-#PUSHOVER_ALARM_MESSAGE="MOTION ON!!!"
 PUSHOVER_NOTIFICATION_TITLE="Hue"
 PUSHOVER_NOTIFICATION_MESSAGE_ON="Home ON"
 PUSHOVER_NOTIFICATION_MESSAGE_OFF="Home OFF"
 
 HOME_STATE=0 # Assumes home is off when script starts. Will correct itself
-#MOTION_STATE=0 # Assumes home is off when script starts. Will correct itself
 
 SKIP=1
 
@@ -25,10 +21,8 @@ echo "$(date): Script started with IP ${HUE_IP} and user ${HUE_USER}" >> HueLog
 while true; do
 
 HOME_OLD=${HOME_STATE}
-#MOTION_OLD=${MOTION_STATE}
 
-CHECK_LIGHTS=$(curl -s -silent -H "Accept: application/json" -X GET http://${HUE_IP}/api/${HUE_USER}/sensors/57/ | grep '{"state":{"presence":true')
-#CHECK_MOTION=$(curl -s -silent -H "Accept: application/json" -X GET http://${HUE_IP}/api/${HUE_USER}/sensors/ | grep '{"state":{"presence":true')
+CHECK_LIGHTS=$(curl -s -silent -H "Accept: application/json" -X GET http://${HUE_IP}/api/${HUE_USER}/sensors/43/ | grep '{"state":{"presence":true')
 
 if [ "${CHECK_LIGHTS}" ]; then
   HOME_STATE=1
@@ -36,33 +30,9 @@ else
   HOME_STATE=0
 fi
 
-#if [ "${CHECK_MOTION}" ]; then
-#  MOTION_STATE=1
-#else
-#  MOTION_STATE=0
-#fi
-
-#echo "$(date): Home State was ${HOME_OLD}, is now ${HOME_STATE}. Motion State was ${MOTION_OLD}, is now ${MOTION_STATE}." >> log
-
 if [[ ${HOME_STATE} = ${HOME_OLD} ]]; then
   HOME_STATE=${HOME_OLD}
-  #echo "Home state not changed"
-  #if [[ ${HOME_STATE} = 0 ]]; then
-    #if [[ ${MOTION_STATE} = ${MOTION_OLD} ]]; then
-      #echo "Motion not changed"
-      #MOTION_STATE=${MOTION_OLD}
-    #elif [[ ${SKIP} = 0 ]]; then
-      #echo "Motion changed and not skipping"
-      #if [[ ${MOTION_STATE} = 1 ]]; then
-        #echo "$(date): ALARM: Motion is ON" >> HueLog
-        #curl -s -silent -F "token=${PUSHOVER_TOKEN}" -F "user=${PUSHOVER_USER}" -F "title=${PUSHOVER_ALARM_TITLE}" -F "message=${PUSHOVER_ALARM_MESSAGE}" https://api.pushover.net/1/messages.json
-        #curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/lights/33/state
-      #fi
-    #fi
-  #fi
 elif [[ ${SKIP} = 0 ]]; then
-#if [[ ${SKIP} = 0 ]]; then
-  #echo "Home state changed and not skipping"
   if [[ ${HOME_STATE} = 1 ]]; then
     echo "$(date): Home state changed to ON" >> HueLog
     curl -s -silent -F "token=${PUSHOVER_TOKEN}" -F "user=${PUSHOVER_USER}" -F "title=${PUSHOVER_NOTIFICATION_TITLE}" -F "message=${PUSHOVER_NOTIFICATION_MESSAGE_ON}" https://api.pushover.net/1/messages.json
@@ -70,30 +40,35 @@ elif [[ ${SKIP} = 0 ]]; then
     echo "$(date): Home state changed to OFF" >> HueLog
     curl -s -silent -F "token=${PUSHOVER_TOKEN}" -F "user=${PUSHOVER_USER}" -F "title=${PUSHOVER_NOTIFICATION_TITLE}" -F "message=${PUSHOVER_NOTIFICATION_MESSAGE_OFF}" https://api.pushover.net/1/messages.json
   fi
-  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/7/config
-  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/8/config
-  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/41/config
-  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/42/config
+  
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/11/config
   curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/12/config
   curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/13/config
-  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/18/config
-  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/19/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/15/config
   curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/16/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/17/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/19/config
   curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/20/config
-  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/26/config
-  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/27/config
-  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/22/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/21/config
   curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/23/config
-  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/30/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/24/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/25/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/27/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/28/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/29/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/31/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/32/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/33/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/35/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/36/config
   curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/37/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/39/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/40/config
+  curl -s -silent -H "Accept: application/json" -X PUT --data '{"on":true}' http://${HUE_IP}/api/${HUE_USER}/sensors/41/config
 fi
 
 SKIP=0
 
-#echo "$(date): NEXT ROUND......in ${DELAY_BETWEEN_CHECKS} seconds" >> log
-
 sleep ${DELAY_BETWEEN_CHECKS}
-
-#echo "$(date): Starting now..." >> log
 
 done # End of perpetual loop
