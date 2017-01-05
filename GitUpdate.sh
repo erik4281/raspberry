a59ccc1a3a0331b44c5b0465a41384aed9df86ec
erik4281 <erik4281@users.noreply.github.com>
2017-01-05 18:01:43 +0100

    Update GitUpdate.sh

--- GitUpdate.sh
+++ GitUpdate.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+VERSION="1701.4"
+
 sleep 5
 sudo git reset --hard HEAD
 sudo git pull
 sudo chmod -R 755 /home/pi/HarmonyHubControl/
 sudo chmod -R 755 /home/pi/
-echo "$(date): Git Update performed" >> GitLog
+
+echo "$(date): Git Update performed using version ${VERSION}" >> GitLog
