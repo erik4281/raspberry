dd12793c4f558e9e2c225dec14b5afe575ee3a4b
erik4281 <erik4281@users.noreply.github.com>
2017-02-24 20:35:07 +0100

    Update HueDownload.sh

--- HueDownload.sh
+++ HueDownload.sh
@@ -2,19 +2,19 @@
 
 VERSION="1708.5"
 
-DELAY_BETWEEN_CHECKS=5
+#DELAY_BETWEEN_CHECKS=5
 
-sleep 10
+#sleep 10
 
 HUE_IP=$(curl https://www.meethue.com/api/nupnp 2> /dev/null | jq -r ".[0].internalipaddress")
 HUE_USER=vSuYBJAton1scEhPyDf4ep85GgmRyFvjJfBjYHIC
 
 echo "$(date): Script version ${VERSION} started with IP ${HUE_IP} and user ${HUE_USER}" >> HueDownloadLog
 
-while true; do
+#while true; do
 
 curl -s -silent -H "Accept: application/json" -X GET http://${HUE_IP}/api/${HUE_USER}/ | tr '},"' '\n' > HueState
 
-sleep ${DELAY_BETWEEN_CHECKS}
+#sleep ${DELAY_BETWEEN_CHECKS}
 
-done
+#done
