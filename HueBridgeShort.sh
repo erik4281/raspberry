38dd0b17e782d509def38f8ea815122bd1ae774f
erik4281 <erik4281@users.noreply.github.com>
2017-07-28 18:31:51 +0200

    Update HueBridgeShort.sh

--- HueBridgeShort.sh
+++ HueBridgeShort.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="1717.7"
+VERSION="1730.5"
 
 HUE_IP=$(curl https://www.meethue.com/api/nupnp 2> /dev/null | jq -r ".[0].internalipaddress")
 HUE_USER=vSuYBJAton1scEhPyDf4ep85GgmRyFvjJfBjYHIC
